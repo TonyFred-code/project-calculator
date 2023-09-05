d57964b33f2f86dc70a308502d91f742051775c8
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-09-05 04:14:44 +0100

    Add more worker functions and buttons

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const keypad = document.querySelector(".keypad");
+const buttons = keypad.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const value = button.getAttribute("data-value");
+    console.log(button, value);
+  });
+});
+
+const calculatorState = {
+  firstOperand: null,
+  secondOperand: null,
+  operate: null,
+  result: null,
+  expression: null,
+};
+
+function updateResult() {}
+function updateExpression() {}
+
 function add(a, b) {
   return a + b;
 }
@@ -12,4 +32,32 @@ function multiply(a, b) {
 
 function divide(a, b) {
   return a / b;
+}
+
+function operate(firstOperand, operator, secondOperand) {
+  let result = 0;
+  switch (operator) {
+    case "+":
+      result = add(firstOperand, secondOperand);
+      break;
+    case "*":
+      result = multiply(firstOperand, secondOperand);
+      break;
+
+    case "-":
+      result = subtract(firstOperand, secondOperand);
+      break;
+    case "/":
+      if (secondOperand === 0) {
+        console.error("SUCH ISN'T HAPPENING HERE!");
+        return;
+      }
+      result = divide(firstOperand, secondOperand);
+      break;
+    default:
+      console.table(firstOperand, operator, secondOperand);
+      break;
+  }
+
+  console.log(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
 }
