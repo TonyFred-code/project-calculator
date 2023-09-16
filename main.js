aba6dc17ee383dd1aba51a87647b51ae4eba80b9
Tony Fred <111481054+TonyFred-code@users.noreply.github.com>
2023-09-16 08:21:01 +0100

    Work on operand order and operator functions

--- main.js
+++ main.js
@@ -2,17 +2,16 @@ const keypad = document.querySelector(".keypad");
 const buttons = keypad.querySelectorAll("button");
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
-    const value = button.getAttribute("data-value");
-    console.log(button, value);
+    changeCalcState(button);
   });
 });
 
 const calculatorState = {
-  firstOperand: null,
-  secondOperand: null,
-  operate: null,
-  result: null,
-  expression: null,
+  firstOperand: "",
+  secondOperand: "",
+  operator: "",
+  result: "",
+  expression: "",
 };
 
 function updateResult() {}
@@ -59,5 +58,47 @@ function operate(firstOperand, operator, secondOperand) {
       break;
   }
 
-  console.log(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
+  return result;
+}
+
+function changeCalcState(button) {
+  const value = button.getAttribute("data-value");
+
+  if (button.classList.contains("digit")) {
+    if (!calculatorState.operator) {
+      calculatorState.firstOperand += value;
+    } else {
+      calculatorState.secondOperand += value;
+    }
+  } else if (button.classList.contains("operator")) {
+    if (
+      value === "=" &&
+      calculatorState.firstOperand &&
+      calculatorState.secondOperand
+    ) {
+      const firstOperand = Number(calculatorState.firstOperand);
+      const secondOperand = Number(calculatorState.secondOperand);
+      const operator = calculatorState.operator;
+      const result = operate(firstOperand, operator, secondOperand);
+      console.log(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
+    } else if (
+      calculatorState.firstOperand &&
+      calculatorState.secondOperand &&
+      calculatorState.operator
+    ) {
+      const firstOperand = Number(calculatorState.firstOperand);
+      const secondOperand = Number(calculatorState.secondOperand);
+      const operator = calculatorState.operator;
+      const result = operate(firstOperand, operator, secondOperand);
+      console.log(`${firstOperand} ${operator} ${secondOperand} = ${result}`);
+    } else {
+      calculatorState.operator = value;
+    }
+    console.log("operator", value);
+  } else if (button.classList.contains("fnc")) {
+    console.log("function key", value);
+  } else {
+    return;
+  }
+  console.table(calculatorState);
 }
